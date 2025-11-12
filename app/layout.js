870fd8786394761ce0a8740e254149c5331db421
Ahmed_khalil <ahgadoora2002@gmail.com>
2025-11-12 14:24:01 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,14 @@
 import "./globals.css";
-import { Cairo, Aref_Ruqaa_Ink } from "next/font/google";
+import { Cairo } from "next/font/google";
 import { AppProvider } from "./context/AppContext";
 
 const cairo = Cairo({
   subsets: ["arabic"],
   weight: ["400", "700"],
-  variable: "--font-cairo",
-});
-
-const aref = Aref_Ruqaa_Ink({
-  subsets: ["arabic"],
-  weight: ["400"],
-  variable: "--font-aref",
 });
 
 export const metadata = {
-  title: "مركز المصباح للتدريب.",
+  title: "مركز المصباح للتدريب. ",
   description: "موقع تدريب ودورات أونلاين",
 };
 
@@ -26,7 +19,7 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={`cairo.variable{aref.variable} font-sans bg-white text-gray-900`}>
+      <body className={`${cairo.className} bg-white text-gray-900`}>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
