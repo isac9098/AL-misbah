00bf2fdbb9280c23dae05fa937ee57c96c0c53dc
Ahmed_khalil <ahgadoora2002@gmail.com>
2025-11-15 21:54:41 +0200

    WhatsappBubble

--- app/course-schedule/page.jsx
+++ app/course-schedule/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef } from "react";
 import { supabase } from "../lib/supabaseClient";
-import WhatsappBubble from "../components/WhatsappBubble";
+import WhatsappBubble from "@/components/WhatsappBubble";
 import { 
   FaCalendarAlt, 
   FaClock, 
