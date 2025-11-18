cbbada52a9635c543516028d95f80a5c24aa6d81
Ahmed_khalil <ahgadoora2002@gmail.com>
2025-11-18 17:54:59 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,16 @@
 import "./globals.css";
 import { Cairo } from "next/font/google";
 import { AppProvider } from "./context/AppContext";
-import FloatingAdminBubble from "../components/FloatingAdminBubble";
+import dynamic from 'next/dynamic';
 
 const cairo = Cairo({
   subsets: ["arabic"],
   weight: ["400", "700"],
+});
+
+// ✅ تحميل المكون ديناميكياً مع تعطيل SSR
+const FloatingAdminBubble = dynamic(() => import('../components/FloatingAdminBubble'), {
+  ssr: false
 });
 
 export const metadata = {
